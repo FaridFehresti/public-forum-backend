9ba3a4ee83a17eeb93e1844b61a5ba2598943f0c
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2024-05-23 11:32:05 +0330

    added authentication so that password is hashed before saving in the db

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
@@ -9,12 +10,25 @@ export class UserService {
     async getUsers(){
         return this.prisma.user.findMany();
     }
+    async findUserByEmail(email:string, password:string){
+        return this.prisma.user.findUnique({where:{email}})
+    }
     async createUser(email:string , password:string){
+        const hashedPassword = await bcrypt.hash(password, 10)
+
         return this.prisma.user.create({
             data:{
                 email,
-                password
+                password:hashedPassword
             }
         })
+    }
+    async validateUser(email:string, password:string): Promise<any>{
+        const user  =  await this.findUserByEmail(email, password);
+        if(user && await bcrypt.compare(password, user.password)){
+            const {password, ...result} = user;
+            return result;
+        }
+        return null;
     }
 }
